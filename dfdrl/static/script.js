8cfc4f83549b83a6c4ceb146edea219ef5ff268b
Peter Jungers <peter.jungers@pcc.edu>
2023-01-02 19:50:59 -0800

    JS improvements

--- dfdrl/static/script.js
+++ dfdrl/static/script.js
@@ -4,49 +4,55 @@ function set_team_class(active_page_season, team) {
     seasons.forEach(number => {
         instances = document.querySelectorAll(".team-active-" + number);
 
+        /* Adding index number (i.e., team[0]) is a workaround in Jinja2;
+        see seasons.html for more info. */
         instances.forEach(instance => {
             if ("season/" + number === active_page_season
                 && team[0] === "damon") {
                 instance.classList.add("damon");
-                instance.setAttribute("aria-current", "page")
             } else if ("season/" + number === active_page_season
                 && team[0] === "felix") {
                 instance.classList.add("felix");
-                instance.setAttribute("aria-current", "page")
             } else if ("season/" + number === active_page_season
                 && team[0] === "ian") {
                 instance.classList.add("ian");
-                instance.setAttribute("aria-current", "page")
             } else if ("season/" + number === active_page_season
                 && team[0] === "benny") {
                 instance.classList.add("benny");
-                instance.setAttribute("aria-current", "page")
             } else if ("season/" + number === active_page_season
                 && team[0] === "peter") {
                 instance.classList.add("peter");
-                instance.setAttribute("aria-current", "page")
             }
         });
     });
 }
 
-// Can you have set_team_class use this for setting aria?
-// You have both Season active and aria-current and season[num] active as well—need to make Seasons active (highlighted) but not aria-current
-function set_aria_current() {
-    a = document.querySelector(".active");
 
-    if (a.classList.contains("active")) {
-        a.setAttribute("aria-current", "page")
+function set_aria_current() {
+    /* Sets aria-current for active navbar link. Active dropdown link
+    in navbar should not be aria-current, rather, active link within
+    the dropdown should be (as set below). */
+    a = document.querySelector(".nav-link.active");
+    if (!a.classList.contains("dropdown-toggle")) {
+        a.setAttribute("aria-current", "page");
     }
+
+    /* Season links within dropdown menu and sidebar */
+    instances = document.querySelectorAll("a.btn");
+    instances.forEach(instance => {
+        if (instance.classList.contains("damon")
+            || instance.classList.contains("felix")
+            || instance.classList.contains("ian")
+            || instance.classList.contains("benny")
+            || instance.classList.contains("peter")) {
+            instance.setAttribute("aria-current", "page");
+        }
+    })
 }
 
-window.addEventListener("load",
-    set_team_class(active_page_season, team),
-    set_aria_current());
 
-
-
-
-
-
+if (typeof active_page_season !== "undefined") {
+    window.addEventListener("DOMContentLoaded", set_team_class(active_page_season, team));
+}
+window.addEventListener("DOMContentLoaded", set_aria_current());
 
