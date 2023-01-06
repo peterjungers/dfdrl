ab9fed44a8990f82569c795cee57bac8cbd8839c
Peter Jungers <peter.jungers@pcc.edu>
2023-01-05 21:22:05 -0800

    About page finished; JS for vehicles.html begun

--- dfdrl/static/script.js
+++ dfdrl/static/script.js
@@ -1,4 +1,4 @@
-function set_team_class(active_page_season, team) {
+function setTeamClass(active_page_season, team) {
     const seasons = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
     seasons.forEach(season_num => {
@@ -7,19 +7,19 @@ function set_team_class(active_page_season, team) {
         /* Adding index number (i.e., team[0]) is a workaround in Jinja2;
         see seasons.html for more info. */
         instances.forEach(instance => {
-            if ("season/" + number === active_page_season
+            if ("season/" + season_num === active_page_season
                 && team[0] === "damon") {
                 instance.classList.add("damon");
-            } else if ("season/" + number === active_page_season
+            } else if ("season/" + season_num === active_page_season
                 && team[0] === "felix") {
                 instance.classList.add("felix");
-            } else if ("season/" + number === active_page_season
+            } else if ("season/" + season_num === active_page_season
                 && team[0] === "ian") {
                 instance.classList.add("ian");
-            } else if ("season/" + number === active_page_season
+            } else if ("season/" + season_num === active_page_season
                 && team[0] === "benny") {
                 instance.classList.add("benny");
-            } else if ("season/" + number === active_page_season
+            } else if ("season/" + season_num === active_page_season
                 && team[0] === "peter") {
                 instance.classList.add("peter");
             }
@@ -28,7 +28,7 @@ function set_team_class(active_page_season, team) {
 }
 
 
-function set_aria_current() {
+function setAriaCurrent() {
     /* Sets aria-current for active navbar link. Active dropdown link
     in navbar should not be aria-current, rather, active link within
     the dropdown should be (as set below). */
@@ -47,12 +47,71 @@ function set_aria_current() {
             || instance.classList.contains("peter")) {
             instance.setAttribute("aria-current", "page");
         }
-    })
+    });
+}
+
+
+function getTeamRows() {
+    const teamSelect = document.querySelector("#team-select");
+
+    teamSelect.addEventListener("change", () => {
+        let rows = document.querySelectorAll("#vehicle-table tbody tr");
+
+        if (teamSelect.value === "All teams") {
+            rows.forEach(row => {
+                if (row.classList.contains("hidden")) {
+                    row.classList.remove("hidden");
+                }
+            });
+        } else {
+            // The selected value to lowercase equals team CSS class:
+            let team = teamSelect.value.toLowerCase();
+
+            rows.forEach(row => {
+                if (row.classList.contains("hidden")) {
+                    row.classList.toggle("hidden");
+                }
+                if (!row.classList.contains(`${team}`)) {
+                    row.classList.toggle("hidden");
+                }
+            });
+        }
+
+        addRowCount();
+    });
+
+}
+
+
+// Adds count of vehicles beneath "No." header in table rows at vehicles.html:
+function addRowCount() {
+    const visibleRows = document.querySelectorAll("#vehicle-table tbody tr:not(.hidden)");
+    const columnAmount = visibleRows[0].cells.length;
+
+    if (columnAmount === 9) {
+        visibleRows.forEach(row => {
+            row.deleteCell(0);
+        });
+    }
+
+    const count = visibleRows.length;
+    for (let c = 0; c < count; c += 1) {
+        visibleRows[c].insertCell(0).innerText = c + 1;
+    }
 }
 
 
 if (typeof active_page_season !== "undefined") {
-    window.addEventListener("DOMContentLoaded", set_team_class(active_page_season, team));
+    window.addEventListener("DOMContentLoaded", setTeamClass(active_page_season, team));
 }
-window.addEventListener("DOMContentLoaded", set_aria_current());
+window.addEventListener("DOMContentLoaded", setAriaCurrent());
+window.addEventListener("DOMContentLoaded", getTeamRows());
 
+window.addEventListener("DOMContentLoaded", addRowCount());
+
+
+
+// const teams = document.querySelector("#team-btn")
+// teams.addEventListener("click", () => {
+//     getTeamRows()
+// });
